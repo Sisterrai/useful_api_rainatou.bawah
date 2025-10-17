f9eff5ec455c31f0ec07c356bcebc2489f10dd92
Sisterrai <bawahraina@gmail.com>
2025-10-17 12:59:13 +0100

    [ADD]: module activation && desactivation

--- useful_api/app/Http/Controllers/ModuleController.php
+++ useful_api/app/Http/Controllers/ModuleController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
-    //
+    //Show all modules
     public function index(){
         $modules = Modules::all();
         return response($modules,200);
